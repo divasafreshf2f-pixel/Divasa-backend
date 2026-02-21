0ba623b8d6c31a8690cd74132fc5b89293852007
Vijay Krishna <divasafreshf2f@gmail.com>
2026-02-21 12:15:52 +0530

    Fix CORS for Render deployment

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:5174"],
+    origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: [
       "Content-Type",
